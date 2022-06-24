d59008d00fee14e01b179de45ae7a7265ac8f087
Brit <Brit@DESKTOP-BFORP22>
2022-06-24 16:27:33 -0700

    Add project files.

---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,12 @@
+﻿// string interpolation = allows us to insert variables into a string literal
+//                        precede a string literal with $
+//                        {} are placeholders
+
+String firstName = "Brit";
+String lastName = "Campos";
+int age = 21;
+
+Console.WriteLine($"Hello {firstName} {lastName}.");
+Console.WriteLine($"You are {age, 10} years old.");
+
+Console.ReadKey();
